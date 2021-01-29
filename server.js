5fb89f5f2e330a853ec323d99e650a1d7a426182
prahladinala <prahladinala@users.noreply.github.com>
2021-01-29 23:04:52 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
+const app = express()
+
+mongoose.connect(
+  'mongodb+srv://prahlad:Ivamsee@123@shorturl.x02w1.mongodb.net/prahlad?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+)
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'ejs')
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find()
+  res.render('index', { shortUrls: shortUrls })
+})
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl })
+  res.redirect('/')
+})
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+
+  if (shortUrl == null) return res.sendStatus(404)
+  shortUrl.clicks++
+  shortUrl.save()
+
+  res.redirect(shortUrl.full)
+})
+app.listen(process.env.PORT || 5000)
